7c7f00f139fe6f0182ecc107704a2f7c72fdfb55
Matteo <santinimatteo31@gmail.com>
2022-06-16 17:03:52 +0200

    fx

--- frida-functions.js
+++ frida-functions.js
@@ -39,13 +39,13 @@ function goToActivity(package_name, idx_py){
 }
 
 //enumerate apk loaded classes. // TODO: insert find functionality
-function enumerateLoadedClasses(class_name = null){
+function enumerateApplicationClasses(search_keyword){//class_name = null){
   Java.perform(function() {
       Java.enumerateLoadedClasses({
           onMatch: function(className) {
-              // if(className.includes("MainActivity")){
-                console.log(`[CLASS] ${className}.R$layout`);
-              // }
+               if(className.includes(search_keyword)){
+                  console.log(`[CLASS] ${className}`);
+               }
           },
           onComplete: function() {}
       });
@@ -62,9 +62,87 @@ function interceptSQLiteQueries(){
   console.log("[*] SQL Query interceptor activated");
 }
 
+function printRunTimeStrings(){
+  Java.perform(function() {
+    ['java.lang.StringBuilder', 'java.lang.StringBuffer'].forEach(function(clazz, i) {
+      console.log('[?] ' + i + ' = ' + clazz);
+      var func = 'toString';
+      Java.use(clazz)[func].implementation = function() {
+        var ret = this[func]();
+        if (ret.indexOf('') != -1) {
+          // print stacktrace if return value contains specific string
+          Java.perform(function() {
+            var jAndroidLog = Java.use("android.util.Log"), jException = Java.use("java.lang.Exception");
+            console.log( jAndroidLog.getStackTraceString( jException.$new() ) );
+          });
+        }
+        send('[' + i + '] ' + ret);
+        return ret;
+      }
+    });
+  });
+}
+
+function changeLocation(latitude_inp, longitude_inp){
+  Java.perform(() => {
+  	var Location = Java.use('android.location.Location');
+  	Location.getLatitude.implementation = function() {
+      var latitude = parseFloat(latitude_inp)
+  		return latitude;
+  	}
+  	Location.getLongitude.implementation = function() {
+      console.log(longitude);
+      var longitude = parseFloat(longitude_inp)
+  		return longitude;
+  	}
+  })
+}
+
+function bypassFlagSecure(){
+   Java.perform(function() {
+      // https://developer.android.com/reference/android/view/WindowManager.LayoutParams.html#FLAG_SECURE
+      var FLAG_SECURE = 0x2000;
+
+      var Runnable = Java.use("java.lang.Runnable");
+      var DisableSecureRunnable = Java.registerClass({
+         name: "me.bhamza.DisableSecureRunnable",
+         implements: [Runnable],
+         fields: {
+            activity: "android.app.Activity",
+         },
+         methods: {
+            $init: [{
+               returnType: "void",
+               argumentTypes: ["android.app.Activity"],
+               implementation: function (activity) {
+                  this.activity.value = activity;
+               }
+            }],
+            run: function() {
+               var flags = this.activity.value.getWindow().getAttributes().flags.value; // get current value
+               flags &= ~FLAG_SECURE; // toggle it
+               this.activity.value.getWindow().setFlags(flags, FLAG_SECURE); // disable it!
+               console.log("Done disabling SECURE flag...");
+            }
+         }
+      });
+
+      Java.choose("com.example.maptmockapplication.MainActivity", {
+       "onMatch": function (instance) {
+            var runnable = DisableSecureRunnable.$new(instance);
+            instance.runOnUiThread(runnable);
+         },
+         "onComplete": function () {}
+      });
+   });
+}
+
 rpc.exports = {
     enumerateactivities: enumerateActivities,
     gotoactivity: goToActivity,
-    enumerateloadedclasses: enumerateLoadedClasses,
+    enumerateapplicationclasses: enumerateApplicationClasses,
     interceptsqlitequeries: interceptSQLiteQueries,
+    printruntimestrings: printRunTimeStrings,
+    changelocation: changeLocation,
+    bypassflagsecure: bypassFlagSecure
 };
--- fridauto_mate.py
+++ fridauto_mate.py
@@ -1,17 +1,19 @@
 import time
 import frida
 import os
+from xml.dom import minidom
+from subprocess import Popen, PIPE
+import re
 
 def my_message_handler(message, payload):
     print(message)
     print(payload)
 
-
 device = frida.get_usb_device()
 package_name = "com.example.maptmockapplication"
-pid = device.spawn([package_name]) #
+pid = device.spawn([package_name])
 device.resume(pid)
-time.sleep(1)  # Without it Java.perform silently fails
+time.sleep(1)
 session = device.attach(pid)
 with open("frida-functions.js") as f:
     script = session.create_script(f.read())
@@ -30,27 +32,102 @@ print(r"""
 
 command = ""
 options = {
-    "1":"Exit", 
-    "2":"Enumerate Activities",
-    "3":"Change Activity By Index",
-    "4":"Enumerate Loaded Classes",
-    "5": "Intercept SQLite Queries"
+    "1":"Exit",
+    "2":"Retrieve APK from device",
+    "3":"Decode APK",
+    "4":"Enumerate & Change Activities",
+    "5":"Enumerate Application Classes",
+    "6":"Intercept SQLite Queries",
+    "7":"Change Location",
 }
+
 options_str = '\n'.join(['[%s] %s' % (key, value) for (key, value) in options.items()])
+arr_activities = []
+
 while 1 == 1:
+    print("[*] ******************************")
     print(options_str)
-    command = input("Enter command:")
+    print("[*] ******************************")
+    command = input("[*] Enter command:")
     if command == "1":
         break
-    elif command == "2":
-        script.exports.enumerateActivities(package_name, 1)
-    elif command == "3":
-        activity_ind = input("Enter the index of the activity that will be displayed: ")
-        script.exports.goToActivity(package_name, activity_ind)
-        os.chdir("C:\platform-tools")
-        os.system('"adb shell input keyevent 3"') # home button
-        os.system(f'"adb shell monkey -p {package_name} -c android.intent.category.LAUNCHER 1"')
-    elif command == "4":
-        script.exports.enumerateLoadedClasses() #todo ricerca classe
-    elif command == "5":
+    if command == "2": # download apk from device
+        print("[*]")
+        print("[*] Retrieving package location...")
+        command = ['adb', 'shell', 'pm', 'path', package_name]
+        process = Popen(command, stdout=PIPE, stderr=True)
+        output, error = process.communicate()
+        location_pkg = output.decode('utf-8').replace('package:', '')
+        print("[*] Package location: " + location_pkg)
+        print("[*] Pulling apk into /analyzed_apks")
+        os.chdir(os.path.dirname(__file__))
+        if not os.path.exists(f'.\\analyzed_apks\\{package_name}'):
+            os.makedirs(f'.\\analyzed_apks\\{package_name}')
+        cmd = f'"adb pull {location_pkg} .\\analyzed_apks\\{package_name}"'.replace('\n','')
+        os.system(cmd)
+        print(f"[*] APK saved in {os.path.dirname(__file__)}\\analyzed_apks\\{package_name}")
+        print("[*]")
+    if command == "3": # B. apktool decode → access to manifest
+        print("[*]")
+        if not os.path.exists(os.path.exists(f'.\\analyzed_apks\\{package_name}\\base.apk')):
+            print("[*] Please, run firstly [2] Retrieve APK from device.")
+        else:
+            if os.path.exists(os.path.exists(f'.\\analyzed_apks\\{package_name}\\base')):
+                decod = input("[*] Decoded files are already present, do you want to repeat the decoding process?[Y/n]")
+                if decod.lower() == "y":
+                    print("[*] Decoding apk...")
+                    os.chdir(os.path.dirname(__file__)+f'\\analyzed_apks\\{package_name}')
+                    cmd = '"apktool d base.apk"'
+                    os.system(cmd)
+            else:
+                print("[*] Decoding apk...")
+                os.chdir(os.path.dirname(__file__)+f'\\analyzed_apks\\{package_name}')
+                cmd = '"apktool d base.apk"'
+                os.system(cmd)
+        print("[*]")
+    if command == "4": # enumerate & change activities
+        if not arr_activities:
+            dom = minidom.parse(f'.\\analyzed_apks\\{package_name}\\base\\AndroidManifest.xml')
+            activities = dom.getElementsByTagName('activity')
+            print(f"There are {len(activities)} activities:")
+            i = 1
+            for activity in activities:
+                pkg_activity = activity.attributes["android:name"].value
+                arr_activities.append(pkg_activity.split('.')[-1])
+                i += 1
+            if i>1:
+                for (i, item) in enumerate(arr_activities, start=0):
+                    print("[" + str(i) + "]  "+item)
+                decod = input("[*] Do you want to swap activity?[Y/n]")
+                if decod.lower() == "y":
+                    activity_ind = input("Enter the index of the activity that will be displayed: ")
+                    activity_ind = int(activity_ind)
+                    if activity_ind < len(arr_activities):
+                        activity = arr_activities[activity_ind]
+                        print(f"adb shell am start -n {package_name}/.{activity}")
+                        os.system(f"adb shell am start -n {package_name}/.{activity}")
+        else:
+            for (i, item) in enumerate(arr_activities, start=0):
+                print("[" + str(i) + "]  "+item)
+            decod = input("[*] Do you want to swap activity?[Y/n]")
+            if decod.lower() == "y":
+                activity_ind = input("Enter the index of the activity that will be displayed: ")
+                activity_ind = int(activity_ind)
+                if activity_ind < len(arr_activities):
+                    activity = arr_activities[activity_ind]
+                    print(f"adb shell am start -n {package_name}/.{activity}")
+                    os.system(f"adb shell am start -n {package_name}/.{activity}")
+                    # am start -n com.example.maptmockapplication/.NotLinkableActivity -- start an activity in adb shell
+    if command == "5":
+        custom_search = input("Enable custom search? [Y/n]: ")
+        if custom_search.lower() == "y":
+            custom_word = input("Insert custom keyword: ")
+            script.exports.enumerateApplicationClasses(custom_word)
+        else:
+            script.exports.enumerateApplicationClasses(package_name)
+    if command == "6":
         script.exports.interceptSQLiteQueries()
+    if command == "7":
+        latitude = input("latitude:")
+        longitude = input("longitude:")
+        script.exports.changeLocation(latitude, longitude)
